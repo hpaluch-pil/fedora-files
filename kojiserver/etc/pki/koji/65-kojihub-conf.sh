851b7c092f88e6538189195e64b45d5fa87e09a8
Henryk Paluch <henryk.paluch@pickering.cz>
2026-09-25 13:38:35 +0200

    Configure WebAuthType and Secret

--- kojiserver/etc/pki/koji/65-kojihub-conf.sh
+++ kojiserver/etc/pki/koji/65-kojihub-conf.sh
@@ -3,11 +3,11 @@ set -xeuo pipefail
 f=/etc/kojiweb/web.conf
 fqdn=`hostname -f`
 kojicert=/etc/pki/koji/certs/kojiadmin.crt
-
-#kojica=/etc/pki/koji/koji_ca_cert.crt
-# s@^\(# \)*\(KojiHubCA *= *\).*@\2'"$kojica"'@' 
+secret=$(openssl rand -hex 10)
 
 sed -i.orig 's@^\(Koji.*URL *= *http://\).*\(/koji.*\)@\1'"$fqdn"'\2@
+     s@^\(# \)*\(WebAuthType *= *\).*@\2ssl@
+     s@^\(# \)*\(Secret *= *\).*@\2'"$secret"'
      s@^\(# \)*\(WebCert *= *\).*@\2'"$kojicert"'@' $f
 diff -u $f{.orig,} || true
 httpd -t
