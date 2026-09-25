1b9ba4b9a17b5f20de888e0b8fc3fa53a9dee1e4
Henryk Paluch <henryk.paluch@pickering.cz>
2026-09-25 10:37:26 +0200

    removed extra space, use localhost as smtp server

--- kojiserver/etc/pki/koji/83-etc-kojid-conf.sh
+++ kojiserver/etc/pki/koji/83-etc-kojid-conf.sh
@@ -4,13 +4,15 @@ f=/etc/kojid/kojid.conf
 fqdn=`hostname -f`
 kojicert=/etc/pki/koji/kojihub.pem
 kojica=/etc/pki/koji/koji_ca_cert.crt
-scms="github.com:/hpaluch-pil/*:no  src.fedoraproject.org:/rpms/python-aexpect.git:no:fedpkg,sources"
+scms="github.com:/hpaluch-pil/*:no src.fedoraproject.org:/rpms/python-aexpect.git:no:fedpkg,sources"
+smtphost=127.0.0.1
 
 sed -i.orig  's@^\(server\|topurl\)\( *= *http://\).*\(/koji.*\)@\1\2'"$fqdn"'\3@
      s@^\(authtype *= *\).*@\1ssl@
      s@^\;\(cert *= *\).*@\1'"$kojicert"'@
      s@^\;\(serverca *= *\).*@\1'"$kojica"'@
      s@^\(allowed_scms *= *\).*@\1'"$scms"'@
+     s@^\(smtphost *= *\).*@\1'"$smtphost"'@
      s@^\;* *\(sleeptime *= *\).*@\11@
      ' $f
 diff -u $f{.orig,} || true
